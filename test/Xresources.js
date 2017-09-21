00f3ab43b91e0b84361479da7a4b3e7e70736670
Florian Beeres <fbeeres@gmail.com>
2017-09-22 00:41:47 +0200

    Xresources tests
    Added all necessary files plus a helpers folder for ava

--- test/Xresources.js
+++ test/Xresources.js
@@ -1,15 +1,18 @@
 const test = require("ava");
 const path = require("path");
 const fs = require("mz/fs");
-const { makeSelector, makeTransforms } = require("../transforms/alacritty");
+const {
+  makeSelectorWord,
+  makeTransforms
+} = require("../transforms/Xresources");
 const transform = require("../transformer");
 const mockThemes = require("./helpers/mockThemes");
 
 const mockConfig = [
   {
-    name: "alacritty",
+    name: "Xresources",
     paths: [
-      path.join(__dirname, "./.config/alacritty/alacritty.yml"),
+      path.join(__dirname, "./.config/Xresources/foo.conf"),
       path.join(__dirname, "foo/foo.yml")
     ],
     makeTransforms
@@ -17,19 +20,16 @@ const mockConfig = [
 ];
 
 test("makeSelector creates correct regexp", t => {
-  const selector = makeSelector("black");
-  const alacrittyColorLine = `black: '0x65737E'`;
-  t.truthy(
-    selector.test(alacrittyColorLine),
-    `Should find ${selector} in ${alacrittyColorLine}`
-  );
+  const selector = makeSelectorWord("foreground");
+  const color = `*.foreground: #65737E`;
+  t.truthy(selector.test(color), `Should find ${selector} in ${color}`);
 });
 
-test("transform transforms duplicate colors (as in alacritty conf)", t => {
+test("transform transforms colors", t => {
   const targetFilePath = mockConfig[0].paths[0];
   const targetFile = fs.readFileSync(targetFilePath, { encoding: "utf8" });
   const expected = fs.readFileSync(
-    path.join(__dirname, ".config/alacritty/alacritty.expected.yml"),
+    path.join(__dirname, ".config/Xresources/foo.expected.conf"),
     { encoding: "utf8" }
   );
 
---
+++ test/helpers/mockThemes.js
@@ -0,0 +1,27 @@
+const mockThemes = [
+  {
+    name: "test",
+    colors: {
+      foreground: "foo",
+      background: "foo",
+      color0: "foo",
+      color1: "foo",
+      color2: "foo",
+      color3: "foo",
+      color4: "foo",
+      color5: "foo",
+      color6: "foo",
+      color7: "foo",
+      color8: "foo2",
+      color9: "foo2",
+      color10: "foo2",
+      color11: "foo2",
+      color12: "foo2",
+      color13: "foo2",
+      color14: "foo2",
+      color15: "foo2"
+    }
+  }
+];
+
+module.exports = mockThemes;
