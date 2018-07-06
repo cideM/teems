9763db612d97e7a5a0d3243d388c1be73051a068
Florian Beeres <fbeeres@gmail.com>
2018-07-06 19:11:13 +0200

    fix cli

--- source/cli/run-cli.js
+++ source/cli/run-cli.js
@@ -24,7 +24,6 @@ const cli = meow(
 		Hit me up on twitter @AyanamiVey or write an issue on https://github.com/cideM/teems-cli
 `,
     {
-        input: ['configPath'],
         flags: {
             list: {
                 type: 'boolean',
@@ -38,26 +37,25 @@ const cli = meow(
     }
 )
 
-if (Object.keys(cli.flags).length > 0) {
-    if (cli.flags.list) {
-        const themes = JSON.parse(fs.readFileSync(cli.input[0]))
-
-        themes.forEach(theme => {
-            console.log(theme.name)
-        })
-    }
-
-    if (cli.flags.dump) {
-        console.log('All supported apps')
-        apps.forEach(app => console.log(app.name))
-    }
-} else if (cli.input.length > 0) {
+if (cli.flags.list) {
     const themes = JSON.parse(fs.readFileSync(cli.input[0]))
+
+    themes.forEach(theme => {
+        console.log(theme.name)
+    })
+}
+
+if (cli.flags.dump) {
+    console.log('All supported apps')
+    apps.forEach(app => console.log(app.name))
+}
+
+if (cli.input.length >= 2) {
+    const themes = JSON.parse(fs.readFileSync(cli.input[0]))
+    console.log(cli.input[0])
     run(apps, themes, cli.input[1]).forEach(p => {
         p.then(result => console.log(`ok: ${result[0]}`)).catch(error => {
             console.error(`not ok: ${error.appName}: ${error.message}`)
         })
     })
-} else {
-    cli.showHelp()
 }
